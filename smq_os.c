d3d347684c61bdf06f061008555b5d12bcaa6f7e
libbylg <libbylg@126.com>
2018-04-06 18:52:36 +0800

    增加构建脚本

--- smq_os.c
+++ smq_os.c
@@ -1,9 +1,11 @@
-#if   defined(_WIN32) && defined(_MSC_VER)
+#if   defined(_WIN32)  && defined(_MSC_VER)
 #include "os/win.msvc/os.c.inc"
-#elif defined(_WIN32) && defined(__GNUC__)
-#include "os/win.gcc/os.c.inc"
-#elif defined(_WIN32) && defined(__clang__)
+#elif defined(_WIN32)  && defined(__GNUC__)
+#include "os/win.mingw/os.c.inc"
+#elif defined(_WIN32)  && defined(__clang__)
 #include "os/win.clang/os.c.inc"
+#elif defined(__linux) && defined(__GNUC__)
+#include "os/linux.gcc/os.c.inc"
 #else
 #error "(os.c.inc)Unsupported compiler or platform"
 #endif
--- smq_os.h
+++ smq_os.h
@@ -1,9 +1,9 @@
 #if   defined(_WIN32)  && defined(_MSC_VER)
 #include "os/win.msvc/os.h"
 #elif defined(_WIN32)  && defined(__GNUC__)
-#include "os/win.gcc/os.h"
+#include "os/win.mingw/os.h"
 #elif defined(_WIN32)  && defined(__clang__)
-#include "os/osx.clang/os.h"
+#include "os/win.clang/os.h"
 #elif defined(__linux) && defined(__GNUC__)
 #include "os/linux.gcc/os.h"
 #else
