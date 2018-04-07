fd65bef340bf5e80ef7308694a724378b0e588b5
luolijun <libbylg@126.com>
2018-04-07 18:36:58 +0800

    refactor configure

--- smq_os.h
+++ smq_os.h
@@ -1,11 +1,13 @@
-#if   defined(_WIN32)  && defined(_MSC_VER)
+#if   defined(_WIN32)    && defined(_MSC_VER)
 #include "os/win.msvc/os.h"
-#elif defined(_WIN32)  && defined(__GNUC__)
+#elif defined(_WIN32)    && defined(__GNUC__)
 #include "os/win.mingw/os.h"
-#elif defined(_WIN32)  && defined(__clang__)
+#elif defined(_WIN32)    && defined(__clang__)
 #include "os/win.clang/os.h"
-#elif defined(__linux) && defined(__GNUC__)
+#elif defined(__linux)   && defined(__GNUC__)
 #include "os/linux.gcc/os.h"
+#elif defined(__APPLE__) && defined(__clang__)
+#include "os/osx.clang/os.h"
 #else
 #error "(os.h)Unsupported compiler or platform"
 #endif
