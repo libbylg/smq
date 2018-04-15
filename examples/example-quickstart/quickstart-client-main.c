f675e86c54bf339362b43ab4d9277a3582df69aa
libbylg <libbylg@126.com>
2018-04-15 19:52:28 +0800

    添加quickstart

--- examples/example-quickstart/quickstart-client-main.c
+++ examples/example-quickstart/quickstart-client-main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+#include "smq.h"
 
 int main(char* args[])
 {
+    smq_inst inst = SMQ_INST_NULL;
+    smq_errno err = smq_open("quick-start", SMQ_ROLE_LEADER, &inst);
+    if (SMQ_OK != err)
+    {
+
+    }
+
     return 0;
 }
 
--- examples/example-quickstart/quickstart-server-main.c
+++ examples/example-quickstart/quickstart-server-main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "smq.h"
 
 int main(char* args[])
 {
